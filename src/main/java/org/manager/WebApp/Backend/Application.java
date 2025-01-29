d166311b8b5272ced47b431feb0df4a5adc29d83
luke0630 <ryuto-0915@outlook.jp>
2025-01-30 04:44:22 +0900

    rm: websocketController

--- src/main/java/org/manager/WebApp/Backend/Application.java
+++ src/main/java/org/manager/WebApp/Backend/Application.java
@@ -7,14 +7,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
-    @Getter
-    static WebsocketController websocketController;
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
                 .properties("server.port=8081")
                 .run(args);
-
-        websocketController = new WebsocketController(1114);
-        websocketController.start();
     }
 }
